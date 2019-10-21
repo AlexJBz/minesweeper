99b73afa9bb5fe3202de44f0858faf58ad9e9810
AlexJBz <alexjkenworthy@gmail.com>
2019-10-21 14:26:23 +0100

    Started work on adding a map to the canvas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 // My personal purple
-let purple = 0x740574;
+let colours = {
+    black: 0x000000,
+    purple: 0x740574
+}
 
 // The map class the inherits from the PIXI.Graphics class
 class Map extends PIXI.Graphics {
-    constructor() {
+    constructor(limit) {
         super();
+        this.limit = limit;
+        this.x = 0;
+        this.y = 0;
+        this.draw();
+    }
 
+    draw() {
+        for (let x = 0; x < this.limit; x++) {
+            for (let y = 0; y < this.limit; y++) {
+                let squareSize = 48;
+                let startPointX = x * squareSize;
+                let startPointY = y * squareSize;
+                this.moveTo(startPointX - 1, startPointY)
+                    .lineStyle(1, colours.black)
+                    .lineTo(startPointX + squareSize, startPointY)
+                    .lineTo(startPointX + squareSize, startPointY + squareSize)
+                    .lineTo(startPointX, startPointY + squareSize)
+                    .lineTo(startPointX, startPointY);
+            }
+        }
     }
 }
 
@@ -14,13 +36,16 @@ let game = {
     pixi: new PIXI.Application({
         width: window.innerWidth,
         height: window.innerHeight,
-        backgroundColor: purple,
+        backgroundColor: colours.purple,
         resolution: 1   
     }),
+    map: new Map(10),
     positionStage () {
         // Sets the stage in the center of the screen instead of top left, this works better in my head when doing coordinates.
         this.pixi.stage.x = window.innerWidth / 2;
         this.pixi.stage.y = window.innerHeight / 2;
+        this.map.x = -this.map.width / 2;
+        this.map.y = -this.map.height / 2;
     },
     init () {
         // Adds the canvas to the body
@@ -31,9 +56,7 @@ let game = {
             this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
             this.positionStage();
         });
-        let square = new PIXI.Graphics();
-        square.beginFill(0x000000).drawRect(-50, -50, 100, 100).endFill();
-        this.pixi.stage.addChild(square);
+        this.pixi.stage.addChild(this.map);
     }
 }
 
